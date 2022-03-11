2f9e00aaeaae04fc1f31c18c07731dacc12ffd41
babornat <natya099@gmail.com>
2022-03-11 14:44:12 +0100

    hotova stranka

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
@@ -9,14 +9,35 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+
     {
-      path: '/about',
-      name: 'about',
+      path: '/article',
+      name: 'article',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      component: () => import('../components/Article.vue')
+    },
+    {
+      path: '/kontakt',
+      name: 'kontakt',
+      component: () => import('../views/Kontakt.vue')
+    },
+    {
+      path: '/rozvoz',
+      name: 'rozvoz',
+      component: () => import('../views/Rozvoz.vue')
+    },
+    {
+      path: '/nabidka',
+      name: 'nabidka',
+      component: () => import('../views/Nabidka.vue')
+    },
+    {
+      path: '/galerie',
+      name: 'galerie',
+      component: () => import('../views/Galerie.vue')
+    },
   ]
 })
 
---
+++ src/views/Rozvoz.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="nabidka">
+
+    <p>Rozvoz zatím není</p>
+
+
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
