57d25124bf774bfb6eebc163fc3c01c8b3cbf0be
babornat <natya099@gmail.com>
2022-03-17 18:11:09 +0100

    kontaktni formular

--- src/views/Kontakt.vue
+++ src/views/Kontakt.vue
@@ -1,19 +1,41 @@
+<!--<template>-->
+<!--  <div class="kontakt">-->
+<!--    <h1>Kontakt</h1>-->
+<!--    <p>Adresa</p>-->
+<!--    <p>Telefon</p>-->
+<!--    <p>IČO</p>-->
+<!--    <p>Soc. sítě</p>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<style>-->
+<!--@media (min-width: 1024px) {-->
+<!--  .about {-->
+<!--    min-height: 100vh;-->
+<!--    display: flex;-->
+<!--    align-items: center;-->
+<!--  }-->
+<!--}-->
+<!--</style>-->
+
 <template>
-  <div class="kontakt">
-    <h1>Kontakt</h1>
-    <p>Adresa</p>
-    <p>Telefon</p>
-    <p>IČO</p>
-    <p>Soc. sítě</p>
+  <div class="row">
+    <div class="col-8 offset-2">
+      <NewArticle></NewArticle>
+    </div>
   </div>
+
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<script>
+import NewArticle from '@/components/NewArticle.vue';
+
+export default {
+  name: "Kontakt",
+  components: {NewArticle}
 }
+</script>
+
+<style scoped>
+
 </style>
