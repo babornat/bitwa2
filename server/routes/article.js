4d01dcc21fb6f9022d6cd87dc52dd58a806a9e73
babornat <natya099@gmail.com>
2022-04-01 16:28:28 +0200

    crud sql

--- server/routes/article.js
+++ server/routes/article.js
@@ -1,80 +1,65 @@
 var express = require('express');
 var router = express.Router();
 
-let articles = [
-    {
-        id: 1,
-        image: 'https://picsum.photos/seed/picsum/500',
-        title: "Title",
-        date: new Date(),
-        text: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam egestas wisi a erat.
-`
-    },
-    {
-        id: 2,
-        image: 'https://picsum.photos/seed/picsum/500',
-        title: "Title",
-        date: new Date(),
-        text: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam egestas wisi a erat.
-`
-    },
-    {
-        id: 3,
-        image: 'https://picsum.photos/seed/picsum/500',
-        title: "Title",
-        date: new Date(),
-        text: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam egestas wisi a erat.
-`
-    },
-];
+const options = {
+    verbose: console.debug
+};
+
+const db = require('better-sqlite3')('articles.sqlite', options);
+
+
 router.get("/", function (req, res, next) {
+   const articles = db.prepare('SELECT * FROM articles').all();
     res.send(articles);
 });
+
 router.get('/:id', (req, res, next) => {
     const id = req.params.id
     console.debug(req.params);
     if (id) {
-        const article = articles.find((a) => a.id === Number.parseInt(id));
+        const article = db.prepare('SELECT * FROM articles WHERE id = ?').get(id);
         res.send(article);
     } else {
         res.send("Not Found");
     }
 });
-    router.get('/:id', (req, res, next) => {
-        const id = req.params.id
-        console.debug(req.params);
-        if (id) {
-            const article = articles.find((a) => a.id === Number.parseInt(id));
-            res.send(article);
-        } else {
-            res.send("Not Found");
-        }
-    });
+    // router.get('/:id', (req, res, next) => {
+    //     const id = req.params.id
+    //     console.debug(req.params);
+    //     if (id) {
+    //         const article = articles.find((a) => a.id === Number.parseInt(id));
+    //         res.send(article);
+    //     } else {
+    //         res.send("Not Found");
+    //     }
+    // });
 
     router.post('/', (req, res, next) => {
         const body = req.body;
         const article = {
-            id:articles.length,
+            image: body.image,
             title: body.title,
-            text:body.text,
-            date: new Date()
+            date: new Date().toISOString(),
+            text:body.text
         };
-        articles.push(article);
-        console.table(articles)
+      const stm = db.prepare("INSERT INTO articles (image, title, date, text) VALUES (?, ?, ?, ?)");
+       stm.run(Object.values(article))
         res.send(article);
 
 
 });
 router.patch("/:id", (req, res) => {
     const body = req.body;
-    const id = req.params.id;
+    const id = req.params.id
+    console.debug(req.params);
     if (id) {
-        const article = articles.find((a) => a.id === Number.parseInt(id));
+        const article = db.prepare('SELECT *FROM articles WHERE articles.id = ?').get(id);
         if(article){
             Object.assign(article, body);
-            res.send(article);
-           // article.title = body.title;
-            console.table(articles);
+           const stm = db.prepare('UPDATE articles SET image = ?, title = ?, date = ?, text = ? WHERE articles.id = ?');
+          console.debug(article);
+         const info = stm.run(article.image,  article.title, article.date, article.text,parseInt(id))
+            res.sendStatus(200)
         }
         else{
             res.sendStatus(404)
@@ -89,8 +74,7 @@ router.delete("/:id", (req, res) => {
 
     const id = req.params.id;
     if (id) {
-         articles = articles.filter((a) => a.id !== Number.parseInt(id));
-        console.table(articles);
+         db.prepare("DELETE FROM articles WHERE id = ?").run(id);
         res.sendStatus(200);
     }else{
     res.sendStatus(404);
